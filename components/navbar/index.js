d21c2c173e357ff0567eb5912f88847435e2d4dc
krishna9304 <krishhtrishh9304@gmail.com>
2021-06-19 12:34:50 +0530

    made the navbar responsive

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -19,6 +19,7 @@ import { useMediaQuery } from "../../pages";
 
 export const NavBar = () => {
   let isPageWide = useMediaQuery("(max-width: 743px)");
+  let [isclose, setclose] = useState(true);
   return (
     <Flex
       justifyContent="space-between"
@@ -34,15 +35,18 @@ export const NavBar = () => {
           url('https://fonts.googleapis.com/css2?family=Fontdiner+Swanky&display=swap');
         </style>
       </Head>
-      <Text
-        p={3}
-        fontSize={isPageWide ? "30px" : "36px"}
-        textShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-        color="#ffffff"
-        fontFamily="Fontdiner Swanky, cursive"
-      >
-        COLLEGE NAME
-      </Text>
+      {isclose ? (
+        <Text
+          p={3}
+          fontSize={isPageWide ? "30px" : "36px"}
+          textShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+          color="#ffffff"
+          fontFamily="Fontdiner Swanky, cursive"
+          width="max-content"
+        >
+          COLLEGE NAME
+        </Text>
+      ) : null}
       {!isPageWide ? (
         <Flex width="50vh" justify="space-between" align="center">
           <Input
@@ -113,71 +117,115 @@ export const NavBar = () => {
         </Flex>
       ) : null}
       {isPageWide ? (
-        <Menu>
-          <MenuButton
-            bg={`url(${"/profile.jpeg"})`}
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            ml={3}
-            mr={3}
-            shadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-            h="40px"
-            borderRadius="50%"
-            as={Button}
-            _active={{
-              bg: `url(${"/profile.jpeg"})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-            }}
-            _hover={{
-              bg: `url(${"/profile.jpeg"})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-            }}
-          ></MenuButton>
-          <MenuList>
-            <MenuGroup title="Options">
-              <Flex>
-                <img
-                  style={{
-                    margin: "10px",
-                    width: "1.5rem",
-                    filter:
-                      "invert() drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
-                  }}
-                  src="/bell-3-128.png"
-                />
-                <img
-                  style={{
-                    margin: "10px",
-                    width: "1.5rem",
-                    filter:
-                      "invert() drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
-                  }}
-                  src="/chat-4-256.png"
-                />
-              </Flex>
-            </MenuGroup>
-            <MenuDivider />
-            <MenuGroup title="Profile">
-              <MenuItem>My Account</MenuItem>
-              <MenuItem>Payments </MenuItem>
-            </MenuGroup>
-            <MenuDivider />
-            <MenuGroup title="Help">
-              <MenuItem>
+        <Flex
+          position="absolute"
+          float="right"
+          justify="flex-end"
+          width="inherit"
+        >
+          {isclose ? (
+            <img
+              onClick={() => {
+                setclose(false);
+              }}
+              style={{
+                cursor: "pointer",
+                margin: "10px",
+                width: "1.5rem",
+              }}
+              src="/search-13-128.png"
+            />
+          ) : (
+            <Flex w="100%" mr="5px">
+              <Input
+                ml={3}
+                mt={1}
+                bg="#E5E5E5"
+                borderRadius="30px"
+                shadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                placeholder="Search"
+                size="sm"
+              />
+              <img
+                onClick={() => {
+                  setclose(true);
+                }}
+                style={{
+                  cursor: "pointer",
+                  margin: "0.8rem",
+                  width: "0.8rem",
+                }}
+                src="/x-mark-128.png"
+              />
+            </Flex>
+          )}
+          <Menu>
+            <MenuButton
+              float="right"
+              bg={`url(${"/profile.jpeg"})`}
+              backgroundRepeat="no-repeat"
+              backgroundSize="cover"
+              ml={3}
+              mr={3}
+              shadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+              h="40px"
+              borderRadius="50%"
+              as={Button}
+              _active={{
+                bg: `url(${"/profile.jpeg"})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+              _hover={{
+                bg: `url(${"/profile.jpeg"})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+            ></MenuButton>
+            <MenuList>
+              <MenuGroup title="Options">
+                <Flex>
+                  <img
+                    style={{
+                      margin: "10px",
+                      width: "1.5rem",
+                      filter:
+                        "invert() drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+                    }}
+                    src="/bell-3-128.png"
+                  />
+                  <img
+                    style={{
+                      margin: "10px",
+                      width: "1.5rem",
+                      filter:
+                        "invert() drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+                    }}
+                    src="/chat-4-256.png"
+                  />
+                </Flex>
+              </MenuGroup>
+              <MenuDivider />
+              <MenuGroup title="Profile">
+                <MenuItem>My Account</MenuItem>
+                <MenuItem>Payments </MenuItem>
+              </MenuGroup>
+              <MenuDivider />
+              <MenuGroup title="Help">
                 <Link href="/">
-                  <a>Docs</a>
+                  <MenuItem>
+                    <a>Docs</a>
+                  </MenuItem>
                 </Link>
-              </MenuItem>
-              <MenuItem>
                 <Link href="/">
-                  <a>FAQ</a>
+                  <MenuItem>
+                    <a>FAQ</a>
+                  </MenuItem>
                 </Link>
-              </MenuItem>
-            </MenuGroup>
-          </MenuList>
-        </Menu>
+              </MenuGroup>
+            </MenuList>
+          </Menu>
+        </Flex>
       ) : null}
     </Flex>
   );
